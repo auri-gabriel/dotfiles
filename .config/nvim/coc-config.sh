9c11f983d1bbd16228d8e43fc34cff9728b29525
Auri Gabriel <ririmello.a@gmail.com>
2023-05-29 18:35:25 -0300

    feat: update script to use nvm

--- .config/nvim/coc-config.sh
+++ .config/nvim/coc-config.sh
@@ -3,12 +3,17 @@
 set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 
-# Install latest nodejs
-if [ ! -x "$(command -v node)" ]; then
-    curl --fail -LSs https://install-node.now.sh/latest | sh
-    export PATH="/usr/local/bin/:$PATH"
-    # Or use package manager, e.g.
-    # sudo apt-get install nodejs
+
+# Install NVM (Node Version Manager) if not already installed
+if ! command -v nvm &> /dev/null; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+fi
+
+# Install the latest version of Node.js
+if ! command -v node &> /dev/null; then
+    nvm install node
 fi
 
 # Use package feature to install coc.nvim
