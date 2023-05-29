b85f79bd57c89d951a3b234ae166fd46512b5824
Auri Gabriel <ririmello.a@gmail.com>
2023-05-29 13:49:20 -0300

    feat: vi mode for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    
+    # Enable Vi mode
+    set -g fish_key_bindings fish_vi_key_bindings
 end
 
 # Startship.rs init
 starship init fish | source
+
