7543ca5ec01e2e7fcc0b4d6ab4c0b2fd7932f4bf
Auri Gabriel <ririmello.a@gmail.com>
2023-06-09 20:38:16 -0300

    add default apps bash config

---
+++ .bashrc.d/default_apps.sh
@@ -0,0 +1,8 @@
+# Set neovim as default editor
+export VISUAL=nvim
+export EDITOR="$VISUAL"
+# Set neovim as manpager
+export MANPAGER='nvim +Man!'
+
+# Set the default texdoc
+export TEXDOCVIEW_pdf="zathura"
